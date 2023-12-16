bf380cb340a40f9be97a5b45bd9796341a4240a8
Scripter1978 <scott.l.griffin@gmail.com>
2023-12-15 19:57:36 -0500

    Initial Commit

---
+++ simple-blazor-htmx-main/BlazorMinimalApiHtmx/HtmlApi/Endpoints.cs
@@ -0,0 +1,26 @@
+﻿using BlazorMinimalApiHtmx.HtmlApi.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMinimalApiHtmx.HtmlApi;
+
+public static class Endpoints
+{
+    public static void Map(WebApplication app)
+    {
+        app.MapGet("/fail",
+            async (BlazorRenderer renderer) =>
+            {
+                await Task.Delay(2000);
+                return Results.Content(await renderer.RenderComponent<FormNoAction>(), "text/html");
+            });
+               
+
+        app.MapGet("/success",
+            async (BlazorRenderer renderer) =>
+            {
+                await Task.Delay(2000);
+                return Results.Content(await renderer.RenderComponent<FormWithAction>(), "text/html");
+            });
+
+    }
+}
